c5ebf59f43dc186285917772dd1225ff5be4f71f
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-04-30 12:06:25 -0300

    depois continua

---
+++ script.js
@@ -0,0 +1,8 @@
+setTimeout(function() {
+    document.querySelector('.myname').classList.add('show');
+  }, 1800);
+  
+  setTimeout(function() {
+    document.querySelector('.dev').classList.add('mostrar');
+  }, 3600);
+  
