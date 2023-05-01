1d834e1a48d91121768d2917604e5dd1a5b5d43b
Ruan Pedro Batista da Silva <121466178+RuanDevz@users.noreply.github.com>
2023-04-30 22:22:12 -0300

    Animação skills

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ setTimeout(function() {
     document.querySelector('.dev').classList.add('mostrar');
   }, 3600);
   
+// Selecione o elemento que deve ser animado
+const allSkills = document.querySelector('.allskills');
+
+// Função que adiciona a classe .visible quando o elemento estiver visível na tela
+function checkScroll() {
+  let elementTop = allSkills.getBoundingClientRect().top;
+  let elementBottom = allSkills.getBoundingClientRect().bottom;
+
+  // Verifique se o elemento está na tela
+  if (elementTop <= window.innerHeight && elementBottom >= 0) {
+    allSkills.classList.add('visible');
+    // Remova o evento de rolagem após a animação ser acionada uma vez
+    window.removeEventListener('scroll', checkScroll);
+  }
+}
+
+// Adicione o evento de rolagem
+window.addEventListener('scroll', checkScroll);
